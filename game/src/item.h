5a4218cafcaf0fdcefe7661d39214775b2f02a95
Muhammet Ali **** <33325253+blackdragonx61@users.noreply.github.com>
2019-02-28 18:23:28 +0300

    Update For PolyItems

--- game/src/item.h
+++ game/src/item.h
@@ -6,8 +6,20 @@
 		{ 
 #ifdef RENEWAL_BOOK_NAME
 			if (!m_pProto) return nullptr;
-			std::string name = CSkillManager::instance().GetSkillName(GetSocket(0)) + ' ' + m_pProto->szLocaleName;
-			return m_dwVnum == 50300 ? name.c_str() : m_pProto->szLocaleName;
+			std::string lname;
+			switch (m_dwVnum) {
+				case 70104:
+					lname = CMobManager::instance().GetMobName(GetSocket(0)) + ' ' + m_pProto->szLocaleName;
+				break;
+				case 50300:
+				case ITEM_SKILLFORGET_VNUM:
+					lname = CSkillManager::instance().GetSkillName(GetSocket(0)) + ' ' + m_pProto->szLocaleName;
+				break;
+				default:
+					lname = m_pProto->szLocaleName;
+				break;
+			}
+			return lname.c_str();
 #else
 			return m_pProto ? m_pProto->szLocaleName : nullptr; 
 #endif
